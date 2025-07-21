1f1250d6d59154d543aa975bfac16deb16ab034f
RahulRaushan1 <rahulraushan137138@gmail.com>
2025-07-21 17:19:03 +0530

    Queue

--- A2_ReverseQueueUsingStack.java
+++ A2_ReverseQueueUsingStack.java
@@ -1,2 +1,41 @@
-package A17_Queue;public class A2_ReverseQueueUsingStack {
-}
+package A17_Queue;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+public class A2_ReverseQueueUsingStack {
+
+        static Queue<Integer> queue;
+        static  void print()
+        {
+            System.out.println(queue);
+        }
+        static void reversequence()
+        {
+            Stack<Integer> st =new Stack<>();
+            while(!queue.isEmpty())
+            {
+                st.push(queue.remove());
+            }
+            while (!st.isEmpty())
+            {
+                queue.add(st.pop());
+            }
+        }
+        public static void main(String[] args) {
+            queue = new LinkedList<>();
+            queue.add(10);
+            queue.add(20);
+            queue.add(30);
+            queue.add(40);
+            queue.add(50);
+            print();
+            reversequence();
+            print();
+
+
+        }
+    }
+
+
+
+
